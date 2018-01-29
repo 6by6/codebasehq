9cc429d835c150029931465406843d4995b13a25
daniel <daniel.r.ness@gmail.com>
2018-01-29 00:20:02 +0000

    added missing serializer imports

--- Entity/Event/WikiPage.php
+++ Entity/Event/WikiPage.php
@@ -3,6 +3,7 @@
 namespace SixBySix\CodebaseHq\Entity\Event;
 
 use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Type;
 use SixBySix\CodebaseHq\Entity\Event;
 
